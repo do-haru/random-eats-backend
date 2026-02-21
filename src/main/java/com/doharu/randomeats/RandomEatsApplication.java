03b2c66b667fc0b8d0fdb4aee71db636c97de4b5
do-haru <doharu05@naver.com>
2026-02-21 23:24:48 +0900

    refactor: 메인 애플리케이션 클래스명 RandomeatsApplication으로 변경

--- src/main/java/com/doharu/randomeats/RandomEatsApplication.java
+++ src/main/java/com/doharu/randomeats/RandomEatsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RandomEatsBackendApplication {
+public class RandomEatsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RandomEatsBackendApplication.class, args);
+		SpringApplication.run(RandomEatsApplication.class, args);
 	}
 
 }
