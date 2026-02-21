314471d963c7c6fdf36659412ef695e2bc061524
do-haru <doharu05@naver.com>
2026-02-21 23:02:09 +0900

    feat: Spring Boot 기본 프로젝트 생성 및 초기 설정

---
+++ src/main/java/com/doharu/randomeats/RandomEatsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.doharu.randomeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomEatsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomEatsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doharu/randomeats/RandomEatsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doharu.randomeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomEatsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
