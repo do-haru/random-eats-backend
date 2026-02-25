56727a701a3448de764fd9bd2731c1fa208bb176
do-haru <doharu05@naver.com>
2026-02-25 15:36:09 +0900

    feat: 카테고리별 메뉴 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,67 @@
 insert into menu (name, category, image_url)
-values ('비빔밥', 'KOREAN', 'url1');
+values ('비빔밥', 'KOREAN', 'https://images.unsplash.com/photo-1741295017668-c8132acd6fc0?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('삼겹살', 'KOREAN', 'https://images.unsplash.com/photo-1709433420601-6e473136571b?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('떡볶이', 'KOREAN', 'https://images.unsplash.com/photo-1679581083578-94eae6e8d7a4?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('삼계탕', 'KOREAN', 'https://images.unsplash.com/photo-1562749606-0a9eb5a8a0f3?q=80&w=1072&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('김밥', 'KOREAN', 'https://images.unsplash.com/photo-1656428254987-45d97432714b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
 insert into menu (name, category, image_url)
-values ('초밥', 'JAPANESE', 'url2');
+values ('초밥', 'JAPANESE', 'https://images.unsplash.com/photo-1563612116625-3012372fccce?q=80&w=741&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('라멘', 'JAPANESE', 'https://images.unsplash.com/photo-1638866281450-3933540af86a?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('우동', 'JAPANESE', 'https://images.unsplash.com/photo-1558985212-324add95595a?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('타코야끼', 'JAPANESE', 'https://images.unsplash.com/photo-1738681335816-8e0df0aa9824?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('오코노미야끼', 'JAPANESE', 'https://plus.unsplash.com/premium_photo-1722593856486-5f87f9fca308?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+
 
 insert into menu (name, category, image_url)
-values ('파스타', 'WESTERN', 'url3');
+values ('파스타', 'WESTERN', 'https://images.unsplash.com/photo-1608897013039-887f21d8c804?q=80&w=692&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('스테이크', 'WESTERN', 'https://plus.unsplash.com/premium_photo-1723672929404-36ba6ed8ab50?q=80&w=1063&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('햄버거', 'WESTERN', 'https://plus.unsplash.com/premium_photo-1683619761492-639240d29bb5?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('리조또', 'WESTERN', 'https://images.unsplash.com/photo-1712407881629-a8ca82a3c64f?q=80&w=693&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('라자냐', 'WESTERN', 'https://plus.unsplash.com/premium_photo-1671559021023-3da68c12aeed?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+
+
+insert into menu (name, category, image_url)
+values ('짜장면', 'CHINESE', 'https://images.unsplash.com/photo-1749880185157-97eacfbfdeea?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('짬뽕', 'CHINESE', 'https://images.unsplash.com/photo-1702737940705-ee75ee0481e6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('볶음밥', 'CHINESE', 'https://plus.unsplash.com/premium_photo-1694141252774-c937d97641da?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('마파두부', 'CHINESE', 'https://plus.unsplash.com/premium_photo-1695151833063-a81c6772de96?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('딤섬', 'CHINESE', 'https://images.unsplash.com/photo-1691995989456-6a77273f9f0c?q=80&w=719&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+
+insert into menu (name, category, image_url)
+values ('아이스크림', 'DESSERT', 'https://plus.unsplash.com/premium_photo-1690440686714-c06a56a1511c?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('와플', 'DESSERT', 'https://plus.unsplash.com/premium_photo-1664478254358-fb8ce668dca6?q=80&w=991&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('빙수', 'DESSERT', 'https://images.unsplash.com/photo-1631148625910-e48b149a64d4?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('팬케이크', 'DESSERT', 'https://plus.unsplash.com/premium_photo-1672846027109-e2c91500afef?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('치즈케이크', 'DESSERT', 'https://images.unsplash.com/photo-1702925614886-50ad13c88d3f?q=80&w=789&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+
+insert into menu (name, category, image_url)
+values ('치킨', 'ETC', 'https://plus.unsplash.com/premium_photo-1683657860968-7474e7ea2d80?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('핫도그', 'ETC', 'https://plus.unsplash.com/premium_photo-1713793236612-50c9bfedbe07?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('샤브샤브', 'ETC', 'https://images.unsplash.com/photo-1614104030967-5ca61a54247b?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('타코', 'ETC', 'https://plus.unsplash.com/premium_photo-1678051141689-1f7a7861b3b4?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+insert into menu (name, category, image_url)
+values ('카레라이스', 'ETC', 'https://images.unsplash.com/photo-1706145779556-f2c642db2699?q=80&w=1331&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
